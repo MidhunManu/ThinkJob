24bbdcd918ebbc1d9540e00996d033080eb81c98
MidhunManu <midhunmanu384@gmail.com>
2026-05-30 20:41:59 +0530

    scaffold app

---
+++ src/main/java/com/ThinkJob/thinkjob/ThinkjobApplication.java
@@ -0,0 +1,13 @@
+package com.ThinkJob.thinkjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ThinkJob/thinkjob/ThinkjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThinkJob.thinkjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
